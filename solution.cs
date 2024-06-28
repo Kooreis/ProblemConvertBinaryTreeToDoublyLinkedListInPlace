7d99db215d59e5b2fb3fb3d8744ecf64111f8b47
Kooreis <kooreis.official@gmail.com>
2024-06-28 14:20:23 -0500

    This update introduces the Node class, which will be used to create the nodes of the binary tree and the doubly linked list. Each node contains an integer data and references to the left and right nodes.

--- solution.cs
+++ solution.cs
@@ -1,6 +1,3 @@
-Here is a C# console application that converts a binary tree to a doubly linked list in place:
-
-```C#
 using System;
 
 public class Node
@@ -14,58 +11,3 @@ public class Node
         left = right = null;
     }
 }
-
-public class BinaryTree
-{
-    Node root;
-
-    void BinaryTree2DoubleLinkedList(Node root, ref Node head)
-    {
-        if (root == null)
-            return;
-
-        BinaryTree2DoubleLinkedList(root.left, ref head);
-
-        if (prev == null)
-            head = root;
-        else
-        {
-            root.left = prev;
-            prev.right = root;
-        }
-        prev = root;
-
-        BinaryTree2DoubleLinkedList(root.right, ref head);
-    }
-
-    Node prev = null;
-
-    void printList(Node node)
-    {
-        while (node != null)
-        {
-            Console.Write(node.data + " ");
-            node = node.right;
-        }
-    }
-
-    public static void Main(String[] args)
-    {
-        BinaryTree tree = new BinaryTree();
-
-        tree.root = new Node(10);
-        tree.root.left = new Node(12);
-        tree.root.right = new Node(15);
-        tree.root.left.left = new Node(25);
-        tree.root.left.right = new Node(30);
-        tree.root.right.left = new Node(36);
-
-        Node head = null;
-        tree.BinaryTree2DoubleLinkedList(tree.root, ref head);
-
-        tree.printList(head);
-    }
-}
-```
-
-This program creates a binary tree and then converts it to a doubly linked list. The conversion is done in place, meaning the original binary tree nodes are used to form the linked list. The linked list is then printed to the console.
