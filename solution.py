503f5565dd503591cee73dab5975cb0f1fb461e9
Kooreis <kooreis.official@gmail.com>
2024-06-29 14:20:23 -0500

    This update introduces the BinaryTree class with a constructor that initializes the root of the tree as None.

--- solution.py
+++ solution.py
@@ -1,5 +1,3 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
+class BinaryTree:
+    def __init__(self):
+        self.root = None
